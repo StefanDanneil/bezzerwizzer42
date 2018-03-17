f5bfeeda1b0c0a17627df881b98ba5830f90a939
stefankarlsson <9807413+StefanKarlsson@users.noreply.github.com>
2018-03-17 18:58:59 +0100

    play around with tooling. Upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -8,20 +8,14 @@ module.exports = {
     filename: 'dist/js/bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
     {
       test: /\.(scss|css)$/,
-      loader: ExtractTextPlugin.extract('css!sass')
-    },
-    {
-      test: /\.png$/,
-      loader: 'file-loader'
-    },
-    { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=dist/fonts/[name].[ext]' },
-    { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=dist/fonts/[name].[ext]' },
-    { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=dist/fonts/[name].[ext]' },
-    { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=dist/fonts/[name].[ext]' },
-    { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=dist/fonts/[name].[ext]' }
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: ['css-loader', 'sass-loader']
+      })
+    }
     ]
   },
   plugins: [
